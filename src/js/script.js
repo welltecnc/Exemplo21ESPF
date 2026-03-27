94c06e0862408a2bd57fe1c8d6cde1dec543eba2
Wellington <welltecnic@hotmail.com>
2026-03-27 09:50:31 -0300

    feat: declaracoes e variaveis

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+// DECLARAÇÕES E VARIAVEIS
+//VAR, LET E CONST
+
+var nome="Fiap";
+console.log(nome);
+var nome="Dev";
+console.log(nome);
+
+if(true){
+    var avatar="Fiapinho";
+}
+
+console.log(avatar);
